85da197428e494e79873a6226a148a7fe546f468
Santhosh <89372815+labsandy@users.noreply.github.com>
2022-01-01 11:04:25 +0530

    Add files via upload

---
+++ jenkins.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+            
+        }
+         stage('Build') {
+            steps {
+                echo 'Hello World'
+            }
+            
+        }
+         stage('Deploy') {
+            steps {
+                echo 'Hello World'
+            }
+            
+        }
+         stage('Test') {
+            steps {
+                echo 'Hello World'
+            }
+            
+        }
+         stage('Releace') {
+            steps {
+                echo 'Hello World'
+            }
+            
+        }
+    }
+}
