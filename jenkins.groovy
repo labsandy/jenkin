eb855afc4707453ff934901d05e7860206308b82
Santhosh <89372815+labsandy@users.noreply.github.com>
2022-01-26 14:17:01 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -10,7 +10,7 @@ pipeline {
         }
          stage('Build') {
             steps {
-                echo 'Hello World'
+                echo 'Hello sandy'
             }
             
         }
